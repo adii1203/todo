c97f98d840c506983c1800c39ef7e1cef5116b66
adii1203 <aditya32ft@gmail.com>
2024-08-06 22:59:41 +0530

    feat: Update Task component to handle checkbox changes

--- components/todos/task.tsx
+++ components/todos/task.tsx
@@ -1,15 +1,22 @@
 // single todo
 
-import { Doc } from "@/convex/_generated/dataModel";
+import { Doc, Id } from "@/convex/_generated/dataModel";
 import React from "react";
 import { Checkbox } from "../ui/checkbox";
 import { cn } from "@/lib/utils";
 
-const Task = ({ data }: { data: Doc<"todos"> }) => {
+const Task = ({
+  data,
+  handelCheck,
+}: {
+  data: Doc<"todos">;
+  handelCheck: (isCompleted: boolean, todoId: Id<"todos">) => void;
+}) => {
   return (
     <div className="mt-4 border-border border-2 rounded px-3 py-2">
       <div className="flex items-center gap-3">
         <Checkbox
+          onCheckedChange={() => handelCheck(data.isCompleted, data._id)}
           checked={data.isCompleted}
           className={cn(
             "rounded-full border-muted-foreground",
--- components/todos/todos.tsx
+++ components/todos/todos.tsx
@@ -1,19 +1,33 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+
 "use client";
 
 import { api } from "@/convex/_generated/api";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import React from "react";
 import Task from "./task";
+import { Id } from "@/convex/_generated/dataModel";
 
 const Todos = () => {
   const todos = useQuery(api.todos.getAllTodos);
+
+  const checkTodo = useMutation(api.todos.checkTodo);
+  const ucCheckTodo = useMutation(api.todos.unCheckTodo);
+
+  const handelCheck = async (isCompleted: boolean, todoId: Id<"todos">) => {
+    if (isCompleted) {
+      await ucCheckTodo({ todoId });
+    } else {
+      await checkTodo({ todoId });
+    }
+  };
 
   if (todos === undefined) return <div>Loading...</div>;
 
   return (
     <div>
       {todos.map((todo) => {
-        return <Task data={todo} key={todo._id} />;
+        return <Task handelCheck={handelCheck} data={todo} key={todo._id} />;
       })}
     </div>
   );
