f96707237eee8b786ada828dc3134bb833af1d25
adii1203 <aditya32ft@gmail.com>
2024-08-08 13:18:37 +0530

    feat: Add getLabels query and deleteLabel mutation for managing labels

--- convex/labels.ts
+++ convex/labels.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { handelUserId } from "./auth";
 
 export const addLabel = mutation({
@@ -15,5 +15,34 @@ export const addLabel = mutation({
       type: "users",
     });
     return labelId;
+  },
+});
+
+export const getLabels = query({
+  handler: async (ctx) => {
+    const userId = await handelUserId(ctx);
+    if (!userId) throw new ConvexError("User is not authenticated");
+
+    const labels = await ctx.db
+      .query("labels")
+      .filter((q) => q.eq(q.field("userId"), userId))
+      .collect();
+
+    return labels;
+  },
+});
+
+export const deleteLabel = mutation({
+  args: { id: v.id("labels") },
+  handler: async (ctx, args) => {
+    const userId = await handelUserId(ctx);
+    try {
+      if (!userId) throw new ConvexError("User is not authenticated");
+
+      await ctx.db.delete(args.id);
+      return true;
+    } catch (error) {
+      return false;
+    }
   },
 });
